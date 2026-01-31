3963e2b27a21a0a1d1fc3016e56e6581cb40cd86
jason-napolitano <jnapolitanoit@gmail.com>
2026-01-31 08:02:22 -0800

    Updated tests

--- tests/Feature/BootstrapTest.php
+++ tests/Feature/BootstrapTest.php
@@ -1,63 +1,15 @@
 <?php
 
-use FrameworkFactory\Contracts\Application\ApplicationInstance;
 use FrameworkFactory\Application;
-use Tests\Providers;
-use Tests\TestState;
+use FrameworkFactory\Contracts;
 
 describe('bootstrap tests', function() {
-
 	test('the application bootstrap build process completes properly', function() {
-		expect(TestState::$app)->toBeInstanceOf(ApplicationInstance::class);
+		expect(Tests\TestState::$app)->toBeInstanceOf(Contracts\Application\ApplicationInstance::class);
 	});
 
 	test('the version number can be assigned to the application', function () {
-		TestState::$app->asVersion('1.2.3');
+		Tests\TestState::$app->asVersion('1.2.3');
 		expect(Application::version())->toBe('1.2.3');
-	});
-
-	test('providers can be successfully added to the container', function() {
-		expect(Application::providers())->toContain(Providers\DeferredServiceProvider::class);
-	});
-
-	test('the cache directory is successfully created', function() {
-		expect(file_exists(TestState::$cachePath))->toBeTrue();
-	});
-
-	test('the cache file is successfully created and exists', function() {
-		expect(file_exists(rtrim(TestState::$cachePath, '/') . '/app.php'))->toBeTrue();
-	});
-
-	test('the cache file includes the standard providers injected upon bootstrap', function() {
-		$file = require rtrim(TestState::$cachePath, '/') . '/app.php';
-
-		$providers = $file['providers'];
-
-		expect($providers)->toContain(Providers\StandardServiceProvider::class);
-
-	});
-
-	test('the cache file includes the deferred providers injected upon bootstrap', function() {
-		$file = require rtrim(TestState::$cachePath, '/') . '/app.php';
-
-		$deferred = $file['deferred'];
-
-		expect($deferred)
-			->toHaveKey('deferred_provider')
-			->and($deferred['deferred_provider'])
-			->toContain(Providers\DeferredServiceProvider::class);
-
-	});
-
-	test('the cache file includes the correct aliases for deferred providers', function() {
-		$file = require rtrim(TestState::$cachePath, '/') . '/app.php';
-
-		$aliases = $file['aliases'];
-
-		expect($aliases)
-			->toHaveKey('deferred_provider')
-			->and($aliases['deferred_provider'])
-			->toEqual('deferred_provider');
-
 	});
 })->group('bootstrap');
